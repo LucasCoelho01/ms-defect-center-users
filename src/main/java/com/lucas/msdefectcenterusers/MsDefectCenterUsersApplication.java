d5bd80f51d9e788582067827434a8d4d9b71950b
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2023-06-11 18:46:54 -0300

    first commit

---
+++ src/main/java/com/lucas/msdefectcenterusers/MsDefectCenterUsersApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.msdefectcenterusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDefectCenterUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDefectCenterUsersApplication.class, args);
+	}
+
+}
